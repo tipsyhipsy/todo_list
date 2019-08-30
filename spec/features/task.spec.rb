329755b0cf5362d4bcee3fe93c6161be97bb3ffc
setsuto horikawa <ralralph@gmail.com>
2019-08-30 20:31:19 +0900

    feturespecのインデント整理

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -19,25 +19,25 @@ RSpec.feature "タスク管理機能", type: :feature do
   end
 
   scenario "タスク作成のテスト" do
-	# new_task_pathにvisitする（タスク登録ページに遷移する）
-  # 1.ここにnew_task_pathにvisitする処理を書く
-	visit new_task_path
-	# 「タスク名」というラベル名の入力欄と、「タスク詳細」というラベル名の入力欄に
-	# タスクのタイトルと内容をそれぞれfill_in（入力）する
-	# 2.ここに「タスク名」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
-	fill_in 'Name', with: "task1"
-	# 3.ここに「タスク詳細」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
-	fill_in 'Description', with: "hoge"
+		# new_task_pathにvisitする（タスク登録ページに遷移する）
+		# 1.ここにnew_task_pathにvisitする処理を書く
+		visit new_task_path
+		# 「タスク名」というラベル名の入力欄と、「タスク詳細」というラベル名の入力欄に
+		# タスクのタイトルと内容をそれぞれfill_in（入力）する
+		# 2.ここに「タスク名」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
+		fill_in 'Name', with: "task1"
+		# 3.ここに「タスク詳細」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
+		fill_in 'Description', with: "hoge"
 
-  # 「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）
-	# 4.「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
-	click_on 'Create Task'
+		# 「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）
+		# 4.「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
+		click_on 'Create Task'
 
-  # clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
-  # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
-	# 5.タスク詳細ページに、テストコードで作成したはずのデータ（記述）がhave_contentされているか（含まれているか）を確認（期待）するコードを書く
-	expect(page).to have_content 'task1'
-	expect(page).to have_content 'hoge'
+		# clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
+		# （タスクが登録されたらタスク詳細画面に遷移されるという前提）
+		# 5.タスク詳細ページに、テストコードで作成したはずのデータ（記述）がhave_contentされているか（含まれているか）を確認（期待）するコードを書く
+		expect(page).to have_content 'task1'
+		expect(page).to have_content 'hoge'
   end
 
 	scenario "タスク詳細のテスト" do
