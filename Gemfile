f0471168adae5bf1133bd6dbe4df72eee1277ab7
setsuto horikawa <ralralph@gmail.com>
2019-08-30 15:10:09 +0900

    FeatureSpec用のgemインストール

--- Gemfile
+++ Gemfile
@@ -36,10 +36,8 @@ end
 
 group :test do
   gem 'capybara', '>= 2.15'
-  gem 'selenium-webdriver'
-  gem 'chromedriver-helper'
+  gem 'webdrivers'
   gem 'factory_bot_rails'
-  gem 'database_cleaner'
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ spec/features/task.spec.rb
@@ -0,0 +1,28 @@
+# このrequireで、Capybaraなどの、Feature Specに必要な機能を使用可能な状態にしています
+require 'rails_helper'
+
+# このRSpec.featureの右側に、「タスク管理機能」のように、テスト項目の名称を書きます（do ~ endでグループ化されています）
+RSpec.feature "タスク管理機能", type: :feature do
+  # scenario（itのalias）の中に、確認したい各項目のテストの処理を書きます。
+	scenario "タスク一覧のテスト" do
+		# あらかじめタスク一覧のテストで使用するためのタスクを二つ作成する
+		Task.create!(name: 'test_task_01', description: 'testtesttest')
+		Task.create!(name: 'test_task_02', description: 'samplesample')
+
+		# tasks_pathにvisitする（タスク一覧ページに遷移する）
+		visit tasks_path
+
+		# visitした（到着した）expect(page)に（タスク一覧ページに）「testtesttest」「samplesample」という文字列が
+		# have_contentされているか？（含まれているか？）ということをexpectする（確認・期待する）テストを書いている
+		expect(page).to have_content 'testtesttest'
+		expect(page).to have_content 'samplesample'
+  end
+
+  scenario "タスク作成のテスト" do
+
+  end
+
+  scenario "タスク詳細のテスト" do
+
+  end
+end
