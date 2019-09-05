7c199a8e0b302d3dedece7a82a38b8f10d3fb1f2
setsuotokohorikawa <ralralph@gmail.com>
2019-09-05 16:08:32 +0900

    期限の入力フォーム作成。一覧に期限の表示を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -40,7 +40,7 @@ class TasksController < ApplicationController
 	private
 
 	def task_params
-		params.require(:task).permit(:name, :description)
+		params.require(:task).permit(:name, :description, :expired_at)
 	end
 
 	def set_task
