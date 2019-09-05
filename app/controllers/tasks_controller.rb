098f7fedf76f6f4721d57b013991edcef5e46842
setsuotokohorikawa <ralralph@gmail.com>
2019-09-05 16:29:18 +0900

    add: 終了期限で並び替えするリンクを一覧画面に追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,11 @@ class TasksController < ApplicationController
 	before_action :set_task, only:[:show, :edit, :update, :destroy]
 
 	def index
-		@tasks = Task.all
+		if params[:sort_expired]
+			@tasks = Task.all.order(expired_at: "ASC")
+		else
+			@tasks = Task.all.order(created_at: "DESC")
+		end
 	end
 
 	def show
