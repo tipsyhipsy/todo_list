8bb8d478bbb6c3019e947b420dde6afefa26e231
setsuotokohorikawa <ralralph@gmail.com>
2019-09-05 15:55:40 +0900

    終了期限のカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_04_022145) do
+ActiveRecord::Schema.define(version: 2019_09_05_061530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_09_04_022145) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "expired_at"
   end
 
 end
