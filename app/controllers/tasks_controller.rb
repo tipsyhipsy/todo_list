a9134570ce02793f96e2a1fa73b28d756bcc128f
setsuotokohorikawa <ralralph@gmail.com>
2019-09-10 11:15:43 +0900

    priorityの登録フォーム作成、indexに表示

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -47,7 +47,7 @@ class TasksController < ApplicationController
 	private
 
 	def task_params
-		params.require(:task).permit(:name, :description, :expired_at, :state)
+		params.require(:task).permit(:name, :description, :expired_at, :state, :priority)
 	end
 
 	def set_task
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
 	validates :name, :description, presence:true
 	enum state:{waiting: 0, working: 1, done: 2}
+	enum priority:{high: 0, medium: 1, low: 2}
 	scope :default, -> { order(created_at: :desc) }
 	scope :expired, -> { order(expired_at: :asc) }
 end
