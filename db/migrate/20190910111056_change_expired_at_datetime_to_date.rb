bd8e0b471c54a72f20a83f4289f96ace5a4523b5
setsuotokohorikawa <ralralph@gmail.com>
2019-09-10 20:13:38 +0900

    change: 期限のデータ型変更

---
+++ db/migrate/20190910111056_change_expired_at_datetime_to_date.rb
@@ -0,0 +1,5 @@
+class ChangeExpiredAtDatetimeToDate < ActiveRecord::Migration[5.2]
+  def change
+    change_column :tasks, :expired_at, :date
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_014540) do
+ActiveRecord::Schema.define(version: 2019_09_10_111056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2019_09_10_014540) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "expired_at"
+    t.date "expired_at"
     t.integer "state", default: 0
     t.integer "priority", default: 0
     t.index ["description"], name: "index_tasks_on_description"
