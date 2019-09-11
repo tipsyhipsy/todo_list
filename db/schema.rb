4ac974cfd40dc5276ea4a519d33334ec4fdc8e7c
setsuotokohorikawa <ralralph@gmail.com>
2019-09-11 15:45:42 +0900

    Userモデルを生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_111056) do
+ActiveRecord::Schema.define(version: 2019_09_11_063550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2019_09_10_111056) do
     t.integer "priority", default: 0
     t.index ["description"], name: "index_tasks_on_description"
     t.index ["name"], name: "index_tasks_on_name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "admin", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
