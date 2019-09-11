7bf06b337832a192f5a7e782556006d39d20a97e
setsuotokohorikawa <ralralph@gmail.com>
2019-09-11 17:02:33 +0900

    usersコントローラー生成、アクション定義

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[edit update show]
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confrimation, :admin)
+  end
+end
