e146ffe3e687efc950814ce2fdcca59c378aebda
setsuotokohorikawa <ralralph@gmail.com>
2019-09-19 10:32:49 +0900

    add:label model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_16_013600) do
+ActiveRecord::Schema.define(version: 2019_09_19_003738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "labels", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", null: false
