3c103ec84bd7b8dea2e90d1a01769b170b56c366
setsuotokohorikawa <ralralph@gmail.com>
2019-09-20 12:28:40 +0900

    add:labelのテストコード作成

--- spec/factories/labels.rb
+++ spec/factories/labels.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :label do
-    name { "MyString" }
+    name { "test1" }
+    id { 1 }
   end
+
+  factory :label_second, class: Label do
+    name { "test2" }
+    id { 2 }
+  end
+
+  factory :label_third, class: Label do
+    name { "test3" }
+    id { 3 }
+  end
+
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,21 @@
 FactoryBot.define do
-  factory :user_first, class: User do
-    name { "foo" }
-    email { "foo@foo.com" }
+  factory :user, class: User do
+    name { 'foo' }
+    email { 'foo@foo.com' }
+    password { 'password' }
+    admin { false }
+  end
+
+  factory :user_other, class: User do
+    name { "baa" }
+    email { "baa@baa.com" }
     password { "password" }
     admin { false }
   end
 
-  factory :user_second, class: User do
-    name { "baa" }
-    email { "baa@baa.com" }
+  factory :user_unique, class: User do
+    sequence(:name) { |n| "user#{n}" }
+    sequence(:email) { |n|"email#{n}@test.com" }
     password { "password" }
     admin { false }
   end
